f3ac850b48e2c4c97a6573318d212e2caf1650a8
shodiqbasiru <shodiqbasiru@gmail.com>
2023-04-09 17:30:54 +0700

    Add Home Scetion and Home Music Section

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,14 +6,23 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
 
+    {{-- Bootstrap --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    {{-- MyCss --}}
+    <link rel="stylesheet" href="{{ url('/assets/css/style.css') }}">
 
 </head>
 <body>
     @include('layouts.navigation')
-    <div class="container">
-        @yield('main')
+    <div class="container-fluid bg-hero">
+        @yield('hero')
     </div>
+
+    <div class="container-fluid h-music" id="h-music">
+        @yield('h-music')
+    </div>
+
 
 
 
